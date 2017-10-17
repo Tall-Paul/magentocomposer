1594f6c0f8985d77842e8e1e052b7a7a38084b0a
Paul Berry <paulberry@PB-Macbook-pro.local>
2017-10-17 11:07:36 +0100

    update install-files file

---
+++ tools/composer/install-files.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+SRCROOT="$(pwd)/../src"
+TARGETROOT="$(pwd)/../http"
+
+find $SRCROOT -type f -print0 | while read -d '' -r file; do
+    #remove src from filepath
+    file=${file/$SRCROOT/}
+    unset array
+    #split remaining file path into array
+    IFS='/' read -r -a array <<< "$file"
+    FILEPATH=''
+    unset array[0] #removes rogue directory seperator
+    #loop through file path
+    for element in "${array[@]}"
+    do
+        FILEPATH=$FILEPATH/$element
+        TARGET=$TARGETROOT$FILEPATH
+        SRC=$SRCROOT$FILEPATH
+
+        if [ -L "${TARGET}" ]; then
+          break
+        fi
+        #if target is a directory, continue loop
+        if [ -d "${TARGET}" ]; then
+            continue
+        fi
+        #if target is a file, remove it and symlink
+        if [ -f "${TARGET}" ]; then
+            if [ -L "${TARGET}" ]; then
+              #it's a link, continue
+              break
+            fi
+            #it's actually a file, remove it and symlinkg
+            echo "removing and symlinking $FILEPATH"
+            rm -f "${TARGET}"
+            ln -s "${SRC}" "${TARGET}"
+            break
+        fi
+        #echo $FILEPATH
+        #if target doesn't exist, create symlink
+        if [ ! -e "${TARGET}" ]; then
+            echo "symlinking $FILEPATH"
+            ln -s "${SRC}" "${TARGET}"
+            break
+        fi
+
+
+    done
+done
